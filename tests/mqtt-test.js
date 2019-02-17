5a57ba64f4b6ed385e91c260d90cd25122c05db6
pedrohbtp <pedrohbtp@gmail.com>
2019-02-17 10:16:30 -0300

    test: added initial travis file

---
+++ tests/mqtt-test.js
@@ -0,0 +1,25 @@
+var mqtt = require('mqtt')
+url = '<your_signed_url>'
+port = 443
+topic = '<your_topic>'
+i = 0
+
+var client  = mqtt.connect(url,
+    {
+        connectTimeout:5*1000,
+        port: port,
+    })
+ 
+client.on('connect', function () {
+  client.subscribe(topic, function (err) {
+    if (!err) {
+      client.publish(topic, 'Hello mqtt')
+    }
+  })
+})
+ 
+client.on('message', function (topic, message) {
+  console.log(message.toString())
+  i = i+1
+  client.publish(topic, 'Hello mqtt '+String(i))
+})
